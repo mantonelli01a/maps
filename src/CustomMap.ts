4475b9f109b687020951b9d70604335d265d67c3
mantonelli01a <mike@mantonelli.plus.com>
2020-05-23 17:50:15 +0100

    add markerContent and added implements clauses

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,9 +1,10 @@
 // Objects can be on the map if they satisfy this interface
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -30,7 +31,7 @@ export class CustomMap {
 
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hi there!',
+        content: mappable.markerContent(),
       });
       infoWindow.open(this.googleMap, marker);
     });
