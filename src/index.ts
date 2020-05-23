8e033682dc3aa662fc454dfd7847c331a873257e
mantonelli01a <mike@mantonelli.plus.com>
2020-05-23 15:59:41 +0100

    adding CustonMap class second attempt

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
-
-console.log(user);
-console.log(company);
-
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const customMap = new CustomMap('map');
