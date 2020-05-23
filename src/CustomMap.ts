55a9958df9d3a075f250a869390e136f2849aa26
mantonelli01a <mike@mantonelli.plus.com>
2020-05-23 16:49:33 +0100

    replaced bad code with an interface for markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
+// Objects can be on the map if they satisfy this interface
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,23 +19,12 @@ export class CustomMap {
     });
   }
 
-  // bad duplicate code
-  addUserMarker(user: User): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
